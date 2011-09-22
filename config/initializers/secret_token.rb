1d75f6de2a8a423c79bafabfb3569e087b09f120
Leo <leo.nie312@gmail.com>
2011-09-22 13:21:49 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiftApp::Application.config.secret_token = 'fce61a2898bbbb14ad2ab7a6a5ee28faeb118ce7ebd7f79585d6fba060c88e3449a3333361e4f6ced13453f886921a369585c95431cc7c1d447e62d882ca12f3'
